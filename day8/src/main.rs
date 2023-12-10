c8ac71291cd80bf4a7da69fe887d5504dd2dacd1
Josh Kropf <josh@slashdev.ca>
2023-12-09 23:25:46 -0500

    day8: part 2 solution

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -3,6 +3,9 @@ use std::{
     collections::HashMap
 };
 
+use num::Integer;
+
+#[derive(Debug)]
 enum Instruction {
     Left,
     Right
@@ -28,11 +31,11 @@ impl Network {
         let mut lines = data.lines();
 
         let inst = lines.next().unwrap();
-        let instructions: Vec<Instruction> = inst.chars()
+        let instructions = inst.chars()
             .map(|c| c.into())
             .collect();
 
-        let mut nodes: HashMap<String, (String, String)> = HashMap::new();
+        let mut nodes = HashMap::new();
 
         for line in lines {
             // skip empty lines
@@ -57,24 +60,50 @@ impl Network {
 
     fn count_steps(&self, start: &str, end: &str) -> usize {
         let mut count = 0;
-
         let mut next = start;
 
-        for inst in &self.instructions {
-            let node = self.nodes.get(next).unwrap();
+        for inst in self.instructions.iter().cycle() {
             count += 1;
 
+            let node = self.nodes.get(next).unwrap();
             next = match inst {
                 Instruction::Left => node.0.as_str(),
                 Instruction::Right => node.1.as_str()
             };
 
-            if next == end {
-                return count;
+            if next.ends_with(end) {
+                break;
             }
         }
 
-        count + self.count_steps(next, end)
+        count
+    }
+
+    // This seems to loop forever. According to what people are saying online,
+    // it might be possible to brute force the solution, but I didn't run
+    // this long enought to find out if it will work/finish.
+    fn _count_steps_multi_start<'a>(&'a self, start: &mut [&'a str], end: &str) -> usize {
+        let mut count = 0;
+
+        let next = start;
+
+        for inst in self.instructions.iter().cycle() {
+            count += 1;
+
+            for label in next.iter_mut() {
+                let node = self.nodes.get(*label).unwrap();
+                *label = match inst {
+                    Instruction::Left => node.0.as_ref(),
+                    Instruction::Right => node.1.as_ref()
+                }
+            }
+
+            if next.iter().all(|n| n.ends_with(end)) {
+                break;
+            }
+        }
+
+        count
     }
 }
 
@@ -84,18 +113,42 @@ fn main() {
     // first arg is command name
     let cmd_name = args.next().unwrap();
 
+    let mut run_part2 = false;
     let input_file = match args.next() {
-        Some(f) => f,
-        None => {
-            print_usage_exit(&cmd_name);
-        }
-    };
+        Some(a) if a == "-p2" => {
+            run_part2 = true;
+            args.next()
+        },
+        Some(a) => Some(a),
+        None => None
+    }.unwrap_or_else(|| print_usage_exit(&cmd_name));
 
     let file_content = fs::read_to_string(input_file)
         .expect("input file should exist and be text file");
 
     let net = Network::load(&file_content);
-    println!("{}", net.count_steps("AAA", "ZZZ"));
+
+    let count = if run_part2 {
+        count_steps_multi_start(&net)
+    } else {
+        net.count_steps("AAA", "ZZZ")
+    };
+
+    println!("{count}");
+}
+
+// Sadly I failed to figure out this on my own.
+// I cheated and looked up how other people solved it.
+fn count_steps_multi_start(net: &Network) -> usize {
+    let start_steps: Vec<_> = net.nodes.keys()
+        .filter(|k| k.ends_with("A"))
+        .map(|k| k.as_str())
+        .collect();
+
+    start_steps.iter()
+        .map(|start| net.count_steps(start, "Z"))
+        .reduce(|a, b| a.lcm(&b))
+        .unwrap()
 }
 
 fn print_usage_exit(me: &str) -> ! {
